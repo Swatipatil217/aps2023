0342bf87cba2f0b343c070c91dcb24988aa3d72a
Swatipatil217 <125040614+Swatipatil217@users.noreply.github.com>
2023-03-28 11:16:52 +0530

    Add files via upload

---
+++ Coin_Combinations_I.cpp
@@ -0,0 +1,28 @@
+//Consider a money system consisting of n coins. Each coin has a positive
+//integer value. Your task is to calculate the number of distinct
+//ways you can produce a money sum x using the available coins.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n, x;
+	cin >> n >> x;
+	int coins[n];
+	for (int i = 0; i < n; i++) { cin >> coins[i]; }
+	int dp[x+1];
+	for (int i = 0; i <= x; i++)
+    {
+        dp[i]=0;
+    }
+	dp[0] = 1;
+	for (int i =1; i<= x; i++) {
+		for (int j =0; j< n; j++) {
+			if (i - coins[j] >= 0) {
+				dp[i] += dp[i- coins[j]];
+			}
+		}
+	}
+	cout << dp[x] << '\n';
+}
